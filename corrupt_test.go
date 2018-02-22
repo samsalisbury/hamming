599be81ca53613019886c3374d3fb97878bcb062
Sam Salisbury <samsalisbury@gmail.com>
2018-02-22 19:41:13 +0000

    Move corruption stuff to own file.

---
+++ corrupt_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"testing"
+)
+
+func randomlyCorrupt(b *byte) {
+	*b = *b + (1 << (rand.Uint64() % 8))
+}
+
+func TestRandomlyCorrupt(t *testing.T) {
+
+	source, err := ioutil.ReadFile("hc.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for n := 1; n <= 10; n++ {
+
+		corrupted := make([]byte, len(source))
+		copy(corrupted, source)
+
+		for i := range corrupted {
+			if i < 2000 {
+				continue
+			}
+			if rand.Intn(10) < 8 {
+				randomlyCorrupt(&corrupted[i])
+			}
+		}
+
+		name := fmt.Sprintf("corrupt-hc-%d.png", n)
+		if err := ioutil.WriteFile(name, corrupted, 0666); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
